b823d2a1cb3defc95e088cb0b3fb70e8a2bfcc4d
ubuntu <ubuntu@gmail.com>
2024-03-31 12:06:24 +0530

    added a delete user popup

--- app/issues/[id]/IssueDeleteButton.tsx
+++ app/issues/[id]/IssueDeleteButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { FC } from "react";
 
 interface Props {
@@ -6,7 +6,33 @@ interface Props {
 }
 
 const IssueDeleteButton: FC<Props> = ({ id }) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete a Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Delete a Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue ? this action cannot be
+          undone
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default IssueDeleteButton;
