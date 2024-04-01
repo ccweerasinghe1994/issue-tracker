8dafe42aaf7c3b33e0f22009ab60eed3f64d09ce
ubuntu <ubuntu@gmail.com>
2024-04-01 18:36:41 +0530

    populate assignee options

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,33 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    getAllUsers();
+  }, []);
+  const usersRows =
+    users &&
+    users.map((user) => (
+      <Select.Item key={user.id} value={user.id}>
+        {user.name}
+      </Select.Item>
+    ));
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Chamara Weerasinghe</Select.Item>
+          {usersRows}
         </Select.Group>
       </Select.Content>
     </Select.Root>
