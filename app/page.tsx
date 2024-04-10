37e6010c6df278f90782e9aa1e60e7a241db522f
ubuntu <ubuntu@gmail.com>
2024-04-10 19:02:08 +0530

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return (
-    <div>
-      hello world 
-    </div>
-  );
+  return <Pagination currentPage={11} itemCount={101} pageSize={10} />;
 }
