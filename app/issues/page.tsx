1816d34ad5e99f7cb8767691a2bbe5e11482ab70
ubuntu <ubuntu@gmail.com>
2024-04-10 17:27:48 +0530

    sort issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,28 +5,61 @@ import { Button, Flex, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import IssueStatusFilter from "./IssueStatusFilter";
 import { FC } from "react";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
   searchParams: {
     status: string;
+    orderBy: keyof Issue;
   };
 }
 
 const IssuesPage: FC<Props> = async ({ searchParams }) => {
-  console.log(
-    "🚀 ~ constIssuesPage:FC<Props>= ~ searchParams:",
-    searchParams.status
-  );
+  const columnHeaders: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    {
+      label: "Issue",
+      value: "title",
+    },
+    {
+      label: "Description",
+      value: "description",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Status",
+      value: "status",
+      className: "hidden md:table-cell",
+    },
+    {
+      label: "Created",
+      value: "createdAt",
+      className: "hidden md:table-cell",
+    },
+  ];
 
   const validStatus: string[] = Object.values(Status);
   const validation = validStatus.includes(searchParams.status);
 
   const passedFilterValue = validation ? searchParams.status : undefined;
 
+  const filterObject =
+    columnHeaders
+      .map((header) => header.value)
+      .includes(searchParams.orderBy) && searchParams.orderBy
+      ? {
+          [searchParams.orderBy]: "asc",
+        }
+      : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status: passedFilterValue as Status,
     },
+    orderBy: filterObject,
   });
 
   const row = (issue: Issue) => (
@@ -61,16 +94,23 @@ const IssuesPage: FC<Props> = async ({ searchParams }) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Description
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              CreatedAt
-            </Table.ColumnHeaderCell>
+            {columnHeaders.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <Link
+                  href={{
+                    query: { ...searchParams, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                </Link>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
